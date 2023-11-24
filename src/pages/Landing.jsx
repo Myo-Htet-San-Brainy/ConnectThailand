d4090aae7155df6f0354058c9abb2834f3415168
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2023-11-24 11:08:00 +0700

    nothing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
   return (
     <div>
       {/* Animation */}
-      {/* <motion.div
+      <motion.div
         className="slide-in"
         initial={{ scaleY: 0 }}
         animate={{ scaleY: 0 }}
@@ -20,7 +20,7 @@ const Landing = () => {
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
         transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-      /> */}
+      />
       {/* TITLE */}
       <SectionTitle text="natural beauty of rural thailand" />
       {/* GALLERY */}
